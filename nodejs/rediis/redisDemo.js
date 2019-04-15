7462bdf26182c0270c405613a87718afdae8838e
mnhmilu <mnh_milu@yahoo.com>
2019-04-15 11:04:54 +0600

    Create redisDemo.js

---
+++ nodejs/rediis/redisDemo.js
@@ -0,0 +1,20 @@
+var redis = require('redis');
+var client = redis.createClient();// add ip here
+
+client.on('connect', function() {
+    console.log('Redis client connected');
+});
+
+client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
+console.log("Setting sample value---->");
+client.set('my test key', 'my test value', redis.print);
+client.get('my test key', function (error, result) {
+    if (error) {
+        console.log(error);
+        throw error;
+    }
+    console.log('GET result ->' + result);
+});
+
