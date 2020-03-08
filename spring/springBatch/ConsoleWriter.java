c154716ca9f3dbe5461a7efeac50abd68d750871
mnhmilu <mnh_milu@yahoo.com>
2020-03-08 16:14:50 +0600

    Create ConsoleWriter.java

---
+++ spring/springBatch/ConsoleWriter.java
@@ -0,0 +1,14 @@
+package com.bkash.template.webapp.utility;
+
+import java.util.List;
+
+import org.springframework.batch.item.ItemWriter;
+
+public class ConsoleItemWriter<T> implements ItemWriter<T> {
+    @Override
+    public void write(List<? extends T> items) throws Exception {
+        for (T item : items) {
+            System.out.println(item.toString());
+        }
+    }
+}
