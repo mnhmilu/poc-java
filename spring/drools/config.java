4e59211d467afb620ec3be0d0b38e623cf328c3d
mnhmilu <mnh_milu@yahoo.com>
2020-04-12 00:05:31 +0600

    Update config.java

--- spring/drools/config.java
+++ spring/drools/config.java
@@ -11,41 +11,31 @@ import org.springframework.context.annotation.Configuration;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+@Configuration
 public class DroolsConfiguration {
-//    private static final String RULES_PATH = "com/rules/";
-    private KieServices kieServices=KieServices.Factory.get();
-    private  KieFileSystem getKieFileSystem() throws IOException{
-        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
+    private static final String RULES_PATH = "rules/";
+    @Bean
+    public KieFileSystem kieFileSystem() throws IOException {
+                KieFileSystem kieFileSystem = getKieServices().newKieFileSystem();
         List<String> rules=Arrays.asList("rules.xls");
         for(String rule:rules){
             kieFileSystem.write(ResourceFactory.newClassPathResource(rule));
         }
         return kieFileSystem;
     }
-    public KieContainer getKieContainer() throws IOException {
-        getKieRepository();
-        KieBuilder kb = kieServices.newKieBuilder(getKieFileSystem());
-        kb.buildAll();
-        KieModule kieModule = kb.getKieModule();
-        KieContainer kContainer = kieServices.newKieContainer(kieModule.getReleaseId());
-        return kContainer;
-    }
-    private void getKieRepository() {
-        final KieRepository kieRepository = kieServices.getRepository();
+    @Bean
+    public KieContainer kieContainer() throws IOException {
+        final KieRepository kieRepository = getKieServices().getRepository();
         kieRepository.addKieModule(new KieModule() {
             public ReleaseId getReleaseId() {
                 return kieRepository.getDefaultReleaseId();
             }
         });
+        KieBuilder kieBuilder = getKieServices().newKieBuilder(kieFileSystem());
+        kieBuilder.buildAll();
+        return getKieServices().newKieContainer(kieRepository.getDefaultReleaseId());
     }
-    public KieSession getKieSession(){
-        getKieRepository();
-        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-        kieFileSystem.write(ResourceFactory.newClassPathResource("rules.xls"));
-        KieBuilder kb = kieServices.newKieBuilder(kieFileSystem);
-        kb.buildAll();
-        KieModule kieModule = kb.getKieModule();
-        KieContainer kContainer = kieServices.newKieContainer(kieModule.getReleaseId());
-        return kContainer.newKieSession();
+    private KieServices getKieServices() {
+        return KieServices.Factory.get();
     }
 }
